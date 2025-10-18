701cfa165ff182eb4adfdaad6d9889e9216ff77d
juraj.pacolt <juraj.pacolt@assecosol.com>
2025-10-19 00:10:29 +0200

    first commit ...

---
+++ app/src/main/java/org/javerland/sockparadox/MainActivity.java
@@ -0,0 +1,30 @@
+package org.javerland.sockparadox;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+                // TODO finish(); ???
+            }
+        });
+    }
+}
