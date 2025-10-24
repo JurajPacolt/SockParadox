4fc55345e79b0a5a4a9cf45c3cd2926569e31be0
juraj.pacolt <juraj.pacolt@assecosol.com>
2025-10-25 00:45:06 +0200

    improvements

--- app/src/main/java/org/javerland/sockparadox/GameActivity.java
+++ app/src/main/java/org/javerland/sockparadox/GameActivity.java
@@ -1,9 +1,15 @@
 package org.javerland.sockparadox;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -15,5 +21,15 @@ public class GameActivity extends AppCompatActivity {
         }
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
+
+        Button actionsButton = findViewById(R.id.btn_actions);
+        actionsButton.setOnClickListener((v) -> {
+            var sheet = new BottomSheetDialog(this);
+            var view = getLayoutInflater().inflate(R.layout.dialog_actions, null);
+            sheet.setContentView(view);
+            sheet.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+            sheet.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            sheet.show();
+        });
     }
 }
